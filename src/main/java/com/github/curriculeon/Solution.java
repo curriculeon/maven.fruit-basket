b872f5e128c898ae83721060e56dab10e351f783
git-leon <xleonhunter@gmail.com>
2022-11-18 14:35:11 -0500

    updated method body

--- src/main/java/com/github/curriculeon/Solution.java
+++ src/main/java/com/github/curriculeon/Solution.java
@@ -1,7 +1,27 @@
 package com.github.curriculeon;
 
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Solution {
     public Integer totalFruit(int[] fruits) {
-        return null;
+        final List<Integer> fruitList = Arrays.stream(fruits).boxed().collect(Collectors.toList());
+        final Set<Integer> fruitTypes = new TreeSet<>(fruitList);
+        final Map<Integer, Integer> fruitTypeCounter = new TreeMap<>();
+        for (Integer fruitType : fruitTypes) {
+            final int numberOfFruit = Collections.frequency(fruitList, fruitType);
+            fruitTypeCounter.put(fruitType, numberOfFruit);
+        }
+        final List<Integer> values = new ArrayList<>(fruitTypeCounter.values());
+        Collections.reverse(values);
+        final Function<Integer, Integer> getValue = (value) -> {
+            try {
+                return values.get(value);
+            } catch(IndexOutOfBoundsException e) {
+                return 0;
+            }
+        };
+        return getValue.apply(0) + getValue.apply(1);
     }
 }
